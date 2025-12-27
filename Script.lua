8c33b380732209dd30da21b80d8705547879bee4
calamityhub657-pixel <calamityhub657@gmail.com>
2025-12-27 02:01:49 -0300

    Refactor script to use dynamic game script mapping

--- Script.lua
+++ Script.lua
@@ -1,7 +1,11 @@
-local PlaceId = 136801880565837
-local BaseURL = "https://raw.githubusercontent.com/calamityhub657-pixel/-----/refs/heads/main/Games/"
-local ScriptName = "Flick.lua"
+local Games = {
+    [136801880565837] = "Flick.lua",
+    [123557829667240] = "Cut%20down%20your%20tree.lua"
+}
 
-if game.PlaceId == PlaceId then
+local BaseURL = "https://raw.githubusercontent.com/calamityhub657-pixel/-----/refs/heads/main/Games/"
+
+local ScriptName = Games[game.PlaceId]
+if ScriptName then
     loadstring(game:HttpGet(BaseURL .. ScriptName))()
 end
